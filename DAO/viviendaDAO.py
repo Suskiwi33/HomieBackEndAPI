14edb15867f5e2d44f66768c7c67406268af2404
Moha Hamra <mohah3000@gmail.com>
2025-12-12 12:05:12 +0100

    Cambios en dao vivienda

--- DAO/viviendaDAO.py
+++ DAO/viviendaDAO.py
@@ -5,14 +5,15 @@ from typing import List
 class ViviendaDAO:
 
     def get_connection(self):
-        
         conn = coneccion_bd()
         if conn is None:
             raise Exception("No se pudo conectar a la base de datos")
         return conn
 
+    # ==========================
     # INSERTAR VIVIENDA
-    def insertVivienda(self, vivienda: Vivienda, userbd: str, passwbd: str) -> bool:
+    # ==========================
+    def insertVivienda(self, vivienda: Vivienda) -> bool:
         sql = """
         INSERT INTO vivienda (
             nombre, balcony, bath_num, `condition`, floor, garage, garden, 
@@ -35,33 +36,39 @@ class ViviendaDAO:
         try:
             cursor.execute(sql, values)
             conn.commit()
+            # Obtenemos el ID generado
             vivienda.setIdVivienda(cursor.lastrowid) 
             return cursor.rowcount == 1
         finally:
             cursor.close()
             conn.close()
 
+    # ==========================
     # SELECT TODAS
-    def selectAllViviendas(self, user_id, userbd: str, passwbd: str) -> List[Vivienda]:
+    # ==========================
+    def selectAllViviendas(self, user_id) -> List[Vivienda]:
+        
         sql = """
             SELECT id, nombre, balcony, bath_num, `condition`, floor, garage, garden, 
                 ground_size, house_type, lift, loc_city, loc_district, loc_neigh, 
                 m2_real, price, room_numbers, swimming_pool, terrace, unfurnished, usuario_id 
             FROM vivienda where usuario_id = %s
         """
-        sql = sql % (user_id,)
+        
+        values = (user_id,) 
 
         conn = self.get_connection()
         cursor = conn.cursor(dictionary=True)
 
         try:
-            cursor.execute(sql)
+            cursor.execute(sql, values)
             results = cursor.fetchall()
 
             viviendas = []
             for row in results:
                 v = Vivienda()
                 v.setIdVivienda(row["id"]) 
+                
                 v.setNombre(row["nombre"])
                 v.setBalcony(row["balcony"])
                 v.setBathNum(row["bath_num"])
@@ -90,10 +97,13 @@ class ViviendaDAO:
             cursor.close()
             conn.close()
 
+    # ==========================
     # DELETE
-    def deleteVivienda(self, vivienda: Vivienda, userbd: str, passwbd: str) -> bool:
+    # ==========================
+    def deleteVivienda(self, vivienda: Vivienda) -> bool:
         sql = "DELETE FROM vivienda WHERE id = %s"
         values = (vivienda.getIdVivienda(),)
+        
         conn = self.get_connection()
         cursor = conn.cursor()
         try:
@@ -104,8 +114,10 @@ class ViviendaDAO:
             cursor.close()
             conn.close()
 
+    # ==========================
     # SELECT BY ID
-    def selectViviendaByID(self, vivienda: Vivienda, userbd: str, passwbd: str) -> Vivienda:
+    # ==========================
+    def selectViviendaByID(self, vivienda: Vivienda) -> Vivienda:
         idVivienda = vivienda.getIdVivienda()
 
         sql = "SELECT id FROM vivienda WHERE id = %s"
