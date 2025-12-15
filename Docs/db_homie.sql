4bc3da2bb6f81a9b03a68e3276d15bc4c411366d
Alex Borda <alexvictorb@gmail.com>
2025-12-15 10:45:18 +0100

    actualizado sql

--- Docs/db_homie.sql
+++ Docs/db_homie.sql
@@ -7,6 +7,7 @@ CREATE TABLE `usuario` (
   `id` int NOT NULL AUTO_INCREMENT,
   `usuario` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
